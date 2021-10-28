f8a5c0705e8844e7903af5aa9f8100be567b7339
zhuliangbin <zhuliangbin@deepdraw.cn>
2021-10-28 15:35:46 +0800

    api文档完成

--- src/main/java/cn/topland/controller/IndexController.java
+++ src/main/java/cn/topland/controller/IndexController.java
@@ -5,11 +5,19 @@ import cn.topland.util.Responses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试
+ */
 @RestController
 public class IndexController {
 
+    /**
+     * 心跳
+     *
+     * @return
+     */
     @GetMapping("/heartbeat")
-    public Response heartbeat() {
+    public Response<String> heartbeat() {
 
         return Responses.success("hello world!");
     }
