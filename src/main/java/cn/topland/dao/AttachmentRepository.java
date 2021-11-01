6a8bdcae85200ef3f5cbedfcb14327cee4d93069
zhuliangbin <zhuliangbin@deepdraw.cn>
2021-11-01 16:05:01 +0800

    20211101:删除附件相关代码

--- src/main/java/cn/topland/dao/AttachmentRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.topland.dao;
-
-import cn.topland.entity.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-}
